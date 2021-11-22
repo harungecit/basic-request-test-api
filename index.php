29b0e6f69369c6ffcb043ebe4763ccf9f9b02e60
Harun Geçit <harungecit@gmail.com>
2021-11-22 12:50:51 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,24 +3,22 @@
 $dateTime = new DateTime('now', new DateTimeZone('America/New_York')); 
 $date = $dateTime->format("H");
 $randSecond = rand(10, 50);
-$usd1 = rand(15,25);
-$usd2 = rand(30,50);
 $data = [];
-for($s=1; $s<=$randSecond;$s++){
+for($s=$randSecond-1; $s<=$randSecond;$s++){
     if($date > 7  && $date < 19){
         //gündüz
-        $data[$s] = $usd1 + ($randSecond / 10);
+        $data[$s] = rand(15,25) + ($randSecond / 10);
 
         /*
         $list=[$username,$adddate,$saves];
-        $sql = "INSERT INTO users (username, adddate, saves) VALUES (?,?,?,?)";
+        $sql = "INSERT INTO users (username, adddate, saves) VALUES (?,?,?)";
         $stmt = $db->prepare($sql);
         $stmt->execute($list);
         */
     }
     else{
         //gece
-        $data[$s] = $usd2 + ($randSecond / 10);
+        $data[$s] = rand(30,50) + ($randSecond / 10);
 
     }
 
